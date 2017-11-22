34652707ebd2ce5235b9e2070dbf7d0eee7795d6
Taylor Monacelli <taylormonacelli@gmail.com>
2017-11-21 18:21:56 -0800

    Noise

---
+++ README.md
@@ -0,0 +1,5 @@
+# streambox_mail
+
+[![Build Status](https://travis-ci.org/TaylorMonacelli/streambox_mail.svg?branch=master)](https://travis-ci.org/TaylorMonacelli/streambox_mail)
+
+Postfix wrapper to add streambox sender
---
+++ metadata.rb
@@ -0,0 +1,20 @@
+name 'streambox_mail'
+maintainer 'Taylor Monacelli'
+maintainer_email 'taylormonacelli@gmail.com'
+license 'All Rights Reserved'
+description 'Installs/Configures streambox_mail'
+long_description 'Installs/Configures streambox_mail'
+version '0.1.0'
+license 'Apache-2.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+issues_url 'https://github.com/streambox/streambox_mail/issues'
+source_url 'https://github.com/streambox/streambox_mail'
+
+depends 'postfix'
+
+supports 'amazon'
+supports 'centos'
+supports 'debian'
+supports 'fedora'
+supports 'opensuse'
+supports 'ubuntu'
